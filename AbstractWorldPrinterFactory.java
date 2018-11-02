636f94fa3eef76c8e0bf1a970c6a90716d9fe339
Marcela <marcela.menezes51@gmail.com>
2018-11-01 21:11:04 -0300

    Exercicio 1.2

--- AbstractWorldPrinterFactory.java
+++
@@ -1,5 +0,0 @@
-package exercicio1_1;
-
-public interface AbstractWorldPrinterFactory {
-	WorldPrinter getPrinterInstance(String printerType);
-}
--- WorldPrinter.java
+++
@@ -1,5 +0,0 @@
-package exercicio1_1;
-
-public interface WorldPrinter {
-	void print();
-}
